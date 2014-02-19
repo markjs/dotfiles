8f2f9d63e3d46a09a405fc9311dfc277f0c13642
Mark J Smith <mark@markjs.net>
2014-02-19 15:43:31 +0000

    Add tswap tmux swap window function

---
+++ tmux/aliases.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+function tswap() {
+  if [ $# = 0 ]; then
+    tmux swap-window -s1 -t2
+  else
+    if [ $# -ne 2 ]; then
+      echo "Illegal number of arguments!"
+    else
+      tmux swap-window -s$1 -t$2
+    fi
+  fi
+}
