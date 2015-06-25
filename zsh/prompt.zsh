893bf9eb7dcd81fb17107bb01a807a2450f714a9
Mark J Smith <mark.smith@futurelearn.com>
2015-06-25 12:49:38 +0100

    Pretty new two-line prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,11 +2,18 @@
 autoload -U colors && colors
 setopt PROMPT_SUBST
 
-# adds the current branch name in green
-git_prompt_info() {
+# adds the current branch name
+git_branch_name() {
   ref=$(git symbolic-ref HEAD 2> /dev/null)
   if [[ -n $ref ]]; then
-    echo "[${ref#refs/heads/}]"
+    echo ${ref#refs/heads/}
+  fi
+}
+
+formatted_git_branch_name() {
+  name=$(git_branch_name)
+  if [[ -n $name ]]; then
+    echo "%F{250}[%F{2}$name%F{250}]$reset_color"
   fi
 }
 
@@ -14,9 +21,11 @@ git_prompt_info() {
 # and using color, see the archlinux zsh guide.
 #
 # https://wiki.archlinux.org/index.php/Zsh#Customizing_your_prompt.
-PROMPT='%~$(git_prompt_info)%(?..%{$fg[red]%})%#%{$reset_color%} '
 
-# Show current user and host when using ssh.
-if [ "$SSH_CONNECTION" ]; then
-  PROMPT="%n@%m:$PROMPT"
-fi
+_newline=$'\n'
+_lineup=$'\e[1A'
+_linedown=$'\e[1B'
+
+PROMPT='%F{21}%~$reset_color$(formatted_git_branch_name)${_newline}%(?..%{%F{9}%})%#%{$reset_color%} '
+
+RPROMPT='%{${_lineup}%}%F{199}%n%F{250}@%F{202}%m%F{250}%{${_linedown}%}'
